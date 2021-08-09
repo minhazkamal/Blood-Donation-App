0e02851d34a47e752c36d3cb78647f38efa4eb01
Minhaz Kamal <70626937+minhazkamal@users.noreply.github.com>
2021-08-10 03:34:34 +0600

    forward geocode and reverse geocode test

---
+++ models/mapbox.js
@@ -0,0 +1,44 @@
+require('dotenv').config();
+var request = require('request');
+
+const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
+const geocodingClient = mbxGeocoding({ accessToken: process.env.MAPBOX_ACCESS_TOKEN });
+
+module.exports.forwardGeocoder = async function(location) {
+	try {	
+		let response = await geocodingClient
+		  .forwardGeocode({
+		    query: location,
+		    limit: 1
+		  })
+		  .send();
+
+		console.log(response.body.features[0].geometry.coordinates);
+	} catch(err) {
+		console.log(err.message);
+	}
+}
+
+module.exports.reverseGeocoder = async function(latitude, longitude) {
+    try{
+        // var url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${longitude},${latitude}.json?access_token=${process.env.MAPBOX_ACCESS_TOKEN}`;
+
+        // request({
+        //     url: url,
+        //     josn: true
+        // }, function (error, response, body) {
+        //     if (!error && response.statusCode === 200) {
+        //         console.log(body) // Print the json response
+        //     }
+        // })
+        let response = await geocodingClient
+        .reverseGeocode({
+            query: [Number(longitude), Number(latitude)],
+          })
+        .send()
+        
+        console.log(response.body);
+    } catch(err) {
+        console.log(err);
+    }
+}
