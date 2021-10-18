f6d466499cd48fa6da73f36bce9fb4b6e499bcb9
Minhaz Kamal <70626937+minhazkamal@users.noreply.github.com>
2021-10-18 09:59:32 +0600

    hyperlink added in logo

--- controllers/landing.js
+++ controllers/landing.js
@@ -1,12 +1,20 @@
-var mysql =require('mysql');
-var express = require ('express');
-var cookie = require ('cookie-parser');
-var db = require ('../models/db_controller');
+var mysql = require('mysql');
+var express = require('express');
+var cookie = require('cookie-parser');
+var db = require('../models/db_controller');
 var router = express.Router();
 
 
-router.get('/',function(req,res){
-    res.render('home.ejs');
+router.get('/', function (req, res) {
+    if (req.session.email != undefined) {
+        db.NotificationUpdateDynamically(req, res)
+            .then(result => {
+                res.render('home.ejs', { email: req.session.email, navbar: req.session.navbar_info, notifications: req.session.notifications});
+            })
+    }
+    else{
+        res.render('home.ejs', {email: 'undefined'});
+    } 
 });
 
 
